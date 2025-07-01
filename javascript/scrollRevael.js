a5324368fd90a513b7c612c74e4a0962103eb8d9
hafiz Ahmad <contact@ahmadproweb.com>
2025-07-01 16:56:14 +0500

    updated full code

---
+++ javascript/scrollRevael.js
@@ -0,0 +1,17 @@
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 10
+});
+ScrollReveal().reveal('.show')
+
+ScrollReveal().reveal('.l', {
+    origin: 'right'
+});
+ScrollReveal().reveal('.r', {
+    origin: 'left'
+});
+ScrollReveal().reveal('.t', {
+    origin: 'top'
+});
